a7b88d5c0e83d8f9f594fc6cacd534f8735d0912
ShirooWas <97787520+ShirooWas@users.noreply.github.com>
2025-08-19 04:25:35 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ async def on_ready():
     print(f"✅ Logged in as {client.user}")
     activity = discord.Activity(
         type=discord.ActivityType.watching,
-        name="over 10M+ IDR secure transactions <a:Reserved:1395938684867707061>"
+        name="over 10M+ IDR secure transactions"
     )
     await client.change_presence(status=discord.Status.online, activity=activity)
 
@@ -156,3 +156,4 @@ async def on_message(message):
         await message.channel.send(embed=embed)
 
 client.run(TOKEN)
+
